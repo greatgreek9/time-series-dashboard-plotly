37b82be084f53dce8f622c027db0d043a41c717e
shubham_kabir <shubhamkabir30@gmail.com>
2021-01-21 18:29:00 +0530

    updated graphs

--- app.py
+++ app.py
@@ -14,31 +14,37 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-app.layout = html.Div(children=[
-                      html.Div(className='row',  # Define the row element
-                               children=[
-                                  html.Div(className='four columns div-user-controls',
-					children = [
+app.layout = html.Div(children = [
 						    html.H2('Dash - STOCK PRICES'),
 						    html.P('''Visualising time series with Plotly - Dash'''),
-						    html.P('''Pick one or more stocks from the dropdown below.''')
-						]),  # Define the left element
-				
-                                  html.Div(className='eight columns div-for-charts bg-grey',
-					children = [
-						dcc.Graph(id='timeseries',
-          config={'displayModeBar': False},
-          animate=True,
-          figure=px.line(df,
-                         x='Date',
-                         y='Close',
-                         template='plotly_dark').update_layout(
-                                   {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-                                    'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-                                    )
-						])  # Define the right element
-                                  ])
-                                ])
+						    html.P('''Pick one or more stocks from the dropdown below.'''),
+                             html.Div(
+                             children = [
+                             dcc.Graph(id='timeseries',
+                             config={'displayModeBar': False},
+                             animate=True,
+                             figure=px.line(df,
+                                    x='Date',
+                                    y='Close',
+                                    template='plotly_dark').update_layout(
+                                                  {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                                   'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+                                                   )
+						]),
+                         html.Div(
+           children = [
+                     dcc.Graph(id='timeseries1',
+                     config={'displayModeBar': False},
+                     animate=True,
+                     figure=px.line(df,
+                            x='Date',
+                            y='Close',
+                            template='plotly_dark').update_layout(
+                                          {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                           'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+                                           )
+                        ])
+                    ])
 
 
 if __name__ == '__main__':
