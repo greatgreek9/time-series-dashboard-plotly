8988d24120e87a4f698aba02236efd652c627721
shubham_kabir <shubhamkabir30@gmail.com>
2021-01-21 16:49:03 +0530

    added gunicorn

--- app.py
+++ app.py
@@ -1,48 +1,29 @@
+import os
+
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
 
-import dash_core_components as dcc
-import plotly.express as px
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-import pandas as pd
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
 
-df = pd.read_csv('data/stockdata2.csv', index_col=0, parse_dates=True)
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
 
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
-# Define the app
-app.layout = html.Div(children=[
-                      html.Div(className='row',  # Define the row element
-                               children=[
-                                  html.Div(className='four columns div-user-controls',
-					children = [
-						    html.H2('Dash - STOCK PRICES'),
-						    html.P('''Visualising time series with Plotly - Dash'''),
-						    html.P('''Pick one or more stocks from the dropdown below.''')
-						]),  # Define the left element
-				
-                                  html.Div(className='eight columns div-for-charts bg-grey',
-					children = [
-						dcc.Graph(id='timeseries',
-          config={'displayModeBar': False},
-          animate=True,
-          figure=px.line(df,
-                         x='Date',
-                         y='value',
-                         color='stock',
-                         template='plotly_dark').update_layout(
-                                   {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-                                    'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-                                    )
-						])  # Define the right element
-                                  ])
-                                ])
-
-
-
-
-
-# Run the app
 if __name__ == '__main__':
     app.run_server(port = 5000, debug=True)
